2e64b8140a6ff492257e64fc8d3e188af9bf9773
VastSea0 <vastseaoffical0@outlook.com>
2024-09-22 23:35:21 +0300

    Refactor converter.rs and add convert_to_json function

--- backend/src/components/converter.rs
+++ backend/src/components/converter.rs
@@ -1,3 +1,10 @@
-fn convert (){
-    todo!("Implement the converter function here!");
-}
+use serde_json::json;
+
+pub fn convert_to_json(id: u32, name: String, request: String) -> String {
+	let json_request = json!({
+		"id": id,
+		"name": name,
+		"request": request
+	});
+	json_request.to_string()
+} 
